1fff5f6bf38e3c8b18abf9d184469eedf61dd03d
jbrownz <jamalbrownbill96@googlemail.com>
2019-08-21 21:11:17 +0100

    bugs fixed

--- server.js
+++ server.js
@@ -17,9 +17,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(morgan('short'));
 
-// if (process.env.NODE_ENV !== 'production') { require('dotenv').config() }
-
-
+//CORS Controll for HTTPS:// requests
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "*");
@@ -33,91 +31,76 @@ app.use(express.static('client/build')); //Middleware
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 
-  // var connection = mysql.createConnection({
-  //   host: 'eu-cdbr-west-02.cleardb.net',
-  //   user: 'bb8dc375142522',                    ONLINE SQL CONNECTION
-  //   password: 'bce7f4ff',
-  //   port: '3306',
-  //   database: 'heroku_c41b8d908c2461c',
+var connection = mysql.createConnection({
+  host: 'us-cdbr-iron-east-02.cleardb.net',
+  user: 'b4b4afbc10b55e',
+  password: '83cf1c87',                 //Currently testing may be fix to heroku error****
+  port: '3306',
+  database: 'heroku_d4f17cbece4a437',
+})
 
-  // })
-
-  // function getConnection() {
-  //   return mysql.createConnection({
-  //     host: 'us-cdbr-iron-east-02.cleardb.net',
-  //     user: 'b4b4afbc10b55e',
-  //     password: '83cf1c87',                     // DEBUGGING******** TURN BACK ON
-  //     port: '3306',
-  //     database: 'heroku_d4f17cbece4a437',
-
-  //   })
-  // }
-
-  // const connection = getConnection();      // DEBUGGING******** TURN BACK ON      
-
-  // let DBTOKEN = "";
-  // connection.connect(function (err) {
-  //   if (!err) {
-  //     console.log("Successfully Connected.");
-  //     var succsToken = "Vodo SQL Live";
-  //     DBTOKEN = succsToken;                        // DEBUGGING******** TURN BACK ON
-
-  //   } else {
-  //     console.log("Failed to connect.");
-  //     var failToken = "SQL Connection failed!!!"
-  //     DBTOKEN = failToken;
-  //   }
-  // });
+connection.on('error', function (err) {                 // DEBUGGING*****REFACTORING *** PROBLEM
+   console.log('caught this error: ' + err.toString());
+});
 
 
-  app.get('/api/responsecheck', (req, res) => {
-    res.send({ express: 'SERVER IS ACTIVE!' });
+app.get('/api/responsecheck', (req, res) => {
+  res.send({ express: 'SERVER IS ACTIVE!' });
+});
+
+app.post('/api/add', (req, res) => {
+  console.log('attempting to create a new user....');
+
+  console.log(req.body.usrname);
+  console.log(req.body.email);
+
+  const Query_Insert = "INSERT INTO register_usr (name, email) VALUES(?, ?)"
+  connection.query(Query_Insert, [req.body.usrname, req.body.email], (err, results, fields) => {
+    if (err) {
+      console.log('Error inserting new user...' + err);
+      res.sendStatus(500);
+      return;
+    } else {                        // DEBUGged ** TICK
+      console.log('New user has successfully been inserted to DB: ', results.insertedId);
+      res.end();
+    }
   });
+});
 
-  app.get('/api/databasecheck', (req, res) => {
-    res.send({ express: DBTOKEN });    // dEBUGGED THIS WORKS******** TURN BACK ON      
-    res.send({ express: 'CURRENTLY OFF FOR DEBUGGING ***  API FOR DB CHECK IS LIVE THOUGH' });
+
+app.get("/api/databasecheck", (req, res) => {
+  const query_ReadAll = "SELECT * FROM register_usr"
+  connection.query(query_ReadAll, function (err, rows, fields) {
+    if (!!err) {
+      res.sendStatus(500);
+      console.log("error in query");
+
+    } else {
+      res.json(rows);
+      console.log("successful query... refer back to browser");
+    }
   });
+});
 
-  app.post('/api/add', (req, res) => {
-    console.log('attempting to create a new user....');
-
-    console.log(req.body.usrname);
-    console.log(req.body.email);
-
-    const Query_Insert = "INSERT INTO register_usr (name, email) VALUES(?, ?)"
-    getConnection().query(Query_Insert, [req.body.usrname, req.body.email], (err, results, fields) => {
-      if (err) {
-        console.log('Error inserting new user...' + err);
-        res.sendStatus(500);
-        return;
-      } else {                        // DEBUGged ** TICK
-        console.log('New user has successfully been inserted to DB: ', results.insertedId);
-        res.end();
-      }
-    });
-  });
-
-
-  app.get("/api/db", (req, res) => {
-    const query_ReadAll = "SELECT * FROM register_usr"
-    getConnection().query(query_ReadAll, function (err, rows, fields) {
-      if (!!err) {
-        res.sendStatus(500);
-        console.log("error in query");
-
-      } else {
-        res.json(rows);
-        console.log("successful query... refer back to browser");
-      }
-    });
-  });
-
-  // Catch all code which routes users back to homepage
-  app.get('*', (req, res) => {
-    res.send(path.join(__dirname, 'client', 'build', 'index.html '));
-    });  
+// Catch all code which routes users back to homepage
+app.get('*', (req, res) => {
+  res.send(path.join(__dirname, 'client', 'build', 'index.html '));
+});
 
 
 
-  process.on('SIGINT', () => { console.log("Bye bye!"); process.exit(); });
+process.on('SIGINT', () => { console.log("Bye bye!"); process.exit(); });
+
+
+// function getConnection() {
+//   return mysql.createConnection({
+//     host: 'us-cdbr-iron-east-02.cleardb.net',
+//     user: 'b4b4afbc10b55e',
+//     password: '83cf1c87',                     // DEBUGGING******** TURN BACK ON
+//     port: '3306',
+//     database: 'heroku_d4f17cbece4a437',
+
+//   })
+// }
+
+// const connection = getConnection();      // DEBUGGING******** TURN BACK ON      
