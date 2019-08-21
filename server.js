338c445e281e5ecb6274172fa214d7f48cccb519
jbrownz <jamalbrownbill96@googlemail.com>
2019-08-21 19:12:09 +0100

    mass node debugging

--- server.js
+++ server.js
@@ -26,95 +26,93 @@ app.use((req, res, next) => {
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
+// app.use(express.static('client/build'));
 
-app.use(express.static('client/build'));
+
 app.get('*', (req, res) => {
   res.send(path.join(__dirname, 'client', 'build', 'index.html '));
-// });  TURN THIS BACK ON WHEN FINISHED
-
-// app.get('*', (req, res) => {
-//   res.send(path.join(__dirname,'client','build','index.html '));
-// });
+  });  
 
 
-// var connection = mysql.createConnection({
-//   host: 'eu-cdbr-west-02.cleardb.net',
-//   user: 'bb8dc375142522',                    ONLINE SQL CONNECTION
-//   password: 'bce7f4ff',
-//   port: '3306',
-//   database: 'heroku_c41b8d908c2461c',
 
-// })
+  // var connection = mysql.createConnection({
+  //   host: 'eu-cdbr-west-02.cleardb.net',
+  //   user: 'bb8dc375142522',                    ONLINE SQL CONNECTION
+  //   password: 'bce7f4ff',
+  //   port: '3306',
+  //   database: 'heroku_c41b8d908c2461c',
 
-// function getConnection() {
-//   return mysql.createConnection({
-//     host: 'us-cdbr-iron-east-02.cleardb.net',
-//     user: 'b4b4afbc10b55e',
-//     password: '83cf1c87',                     // DEBUGGING******** TURN BACK ON
-//     port: '3306',
-//     database: 'heroku_d4f17cbece4a437',
+  // })
 
-//   })
-// }
+  // function getConnection() {
+  //   return mysql.createConnection({
+  //     host: 'us-cdbr-iron-east-02.cleardb.net',
+  //     user: 'b4b4afbc10b55e',
+  //     password: '83cf1c87',                     // DEBUGGING******** TURN BACK ON
+  //     port: '3306',
+  //     database: 'heroku_d4f17cbece4a437',
 
-// const connection = getConnection();      // DEBUGGING******** TURN BACK ON      
+  //   })
+  // }
 
-// let DBTOKEN = "";
-// connection.connect(function (err) {
-//   if (!err) {
-//     console.log("Successfully Connected.");
-//     var succsToken = "Vodo SQL Live";
-//     DBTOKEN = succsToken;                        // DEBUGGING******** TURN BACK ON
+  // const connection = getConnection();      // DEBUGGING******** TURN BACK ON      
 
-//   } else {
-//     console.log("Failed to connect.");
-//     var failToken = "SQL Connection failed!!!"
-//     DBTOKEN = failToken;
-//   }
-// });
+  // let DBTOKEN = "";
+  // connection.connect(function (err) {
+  //   if (!err) {
+  //     console.log("Successfully Connected.");
+  //     var succsToken = "Vodo SQL Live";
+  //     DBTOKEN = succsToken;                        // DEBUGGING******** TURN BACK ON
+
+  //   } else {
+  //     console.log("Failed to connect.");
+  //     var failToken = "SQL Connection failed!!!"
+  //     DBTOKEN = failToken;
+  //   }
+  // });
 
 
-app.get('/api/responsecheck', (req, res) => {
-  res.send({ express: 'SERVER IS ACTIVE!' });
-});
-
-app.get('/api/databasecheck', (req, res) => {
-  res.send({ express: DBTOKEN });    // dEBUGGED THIS WORKS******** TURN BACK ON      
-  res.send({ express: 'CURRENTLY OFF FOR DEBUGGING ***  API FOR DB CHECK IS LIVE THOUGH' });
-});
-
-app.post('/api/add', (req, res) => {
-  console.log('attempting to create a new user....');
-
-  console.log(req.body.usrname);
-  console.log(req.body.email);
-
-  const Query_Insert = "INSERT INTO register_usr (name, email) VALUES(?, ?)"
-  getConnection().query(Query_Insert, [req.body.usrname, req.body.email], (err, results, fields) => {
-    if (err) {
-      console.log('Error inserting new user...' + err);
-      res.sendStatus(500);
-      return;
-    } else {                        // DEBUGged ** TICK
-      console.log('New user has successfully been inserted to DB: ', results.insertedId);
-      res.end();
-    }
+  app.get('/api/responsecheck', (req, res) => {
+    res.send({ express: 'SERVER IS ACTIVE!' });
   });
-});
 
-
-app.get("/api/db", (req, res) => {
-  const query_ReadAll = "SELECT * FROM register_usr"
-  getConnection().query(query_ReadAll, function (err, rows, fields) {
-    if (!!err) {
-      res.sendStatus(500);
-      console.log("error in query");
-
-    } else {
-      res.json(rows);
-      console.log("successful query... refer back to browser");
-    }
+  app.get('/api/databasecheck', (req, res) => {
+    res.send({ express: DBTOKEN });    // dEBUGGED THIS WORKS******** TURN BACK ON      
+    res.send({ express: 'CURRENTLY OFF FOR DEBUGGING ***  API FOR DB CHECK IS LIVE THOUGH' });
   });
-})
 
-process.on('SIGINT', () => { console.log("Bye bye!"); process.exit(); });
+  app.post('/api/add', (req, res) => {
+    console.log('attempting to create a new user....');
+
+    console.log(req.body.usrname);
+    console.log(req.body.email);
+
+    const Query_Insert = "INSERT INTO register_usr (name, email) VALUES(?, ?)"
+    getConnection().query(Query_Insert, [req.body.usrname, req.body.email], (err, results, fields) => {
+      if (err) {
+        console.log('Error inserting new user...' + err);
+        res.sendStatus(500);
+        return;
+      } else {                        // DEBUGged ** TICK
+        console.log('New user has successfully been inserted to DB: ', results.insertedId);
+        res.end();
+      }
+    });
+  });
+
+
+  app.get("/api/db", (req, res) => {
+    const query_ReadAll = "SELECT * FROM register_usr"
+    getConnection().query(query_ReadAll, function (err, rows, fields) {
+      if (!!err) {
+        res.sendStatus(500);
+        console.log("error in query");
+
+      } else {
+        res.json(rows);
+        console.log("successful query... refer back to browser");
+      }
+    });
+  });
+  
+  process.on('SIGINT', () => { console.log("Bye bye!"); process.exit(); });
