4d858103fbefebc5fa270b659d02001da8a3f738
jbrownz <jamalbrownbill96@googlemail.com>
2019-08-27 11:13:42 +0100

    background bug fix 1

--- client/src/components/Reviews.js
+++ client/src/components/Reviews.js
@@ -8,20 +8,23 @@ import ScrollAnimation from 'react-animate-on-scroll'
 export default class Reviews extends React.Component {
     render() {
         return (
-            <div id = "review-wrapper">
+            <div id="review-wrapper">
 
-            <ScrollAnimation delay={10} animateIn="flipInY" duration={8}>
-            <img src = {revone} alt = "amazing reviews" className="revanim"></img>
-            </ScrollAnimation>
+                <div className="hide-mob">
+                    <ScrollAnimation delay={10} animateIn="flipInY" duration={8}>
+                        <img src={revone} alt="amazing reviews" className="revanim"></img>
+                    </ScrollAnimation>
+                </div>
 
-            <ScrollAnimation delay={800} animateIn="flipInX" duration={3}>
-            <img src = {revtwo} alt = "amazing reviews" className="revanim"></img>
-            </ScrollAnimation>
+                <ScrollAnimation delay={800} animateIn="flipInX" duration={3}>
+                    <img src={revtwo} alt="amazing reviews" className="revanim"></img>
+                </ScrollAnimation>
 
-            <ScrollAnimation delay={1000} animateIn="flipInY" duration={6}>
-            <img src = {revthree} alt = "amazing reviews" className="revanim"></img>
-            </ScrollAnimation>
-
+                <div className="hide-mob">
+                    <ScrollAnimation delay={1000} animateIn="flipInY" duration={6}>
+                        <img src={revthree} alt="amazing reviews" className="revanim"></img>
+                    </ScrollAnimation>
+                </div>
             </div>
         )
     }
